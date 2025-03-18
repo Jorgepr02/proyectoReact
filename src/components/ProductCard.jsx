759315c9727e7d2c860a944d6aef895099677553
Jorgepr02 <jorgepampinrios02@liceolapaz.net>
2025-03-19 00:14:31 +0100

    Mejorado estilos home

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,7 @@ const ProductCard = ({ product }) => {
   console.log("🔗 Producto en ProductCard:", product); // Verificar si el ID es válido
 
   return (
-    <div className="border p-4 rounded shadow">
+    <div className="border p-4 bg-white rounded shadow">
       <img src={`http://localhost:5000${product.image}`} alt={product.title} className="w-full h-40 object-cover" />
       <h3 className="text-lg">{product.title}</h3>
       <p className="text-gray-600">${product.price}</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,11 +19,10 @@ const Home = () => {
     );  }
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold">Bienvenido a nuestra Tienda</h1>
-      <p className="text-gray-600 mt-2">Encuentra los mejores productos al mejor precio.</p>
+    <div className="container text-center mx-auto p-4">
+      <h1 className="text-3xl text-white font-bold">HOME</h1>
 
-      <h2 className="text-2xl font-bold mt-6">Productos Destacados</h2>
+      <h2 className="text-2xl text-white font-bold mt-6">Productos Destacados</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
         {featuredProducts.length > 0 ? (
           featuredProducts.map((product) => (
